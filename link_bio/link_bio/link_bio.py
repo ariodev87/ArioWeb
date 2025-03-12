033de7a1cc23c24b3ddbfa942e73c72d86971ad8
ariodev87 <ariel871207@gmail.com>
2025-03-12 11:54:54 +0100

    docker

--- link_bio/link_bio/link_bio.py
+++ link_bio/link_bio/link_bio.py
@@ -14,7 +14,6 @@ app = rx.App(
         "https://fonts.googleapis.com/css2?family=Doto:wght@100..900&family=Press+Start+2P&display=swap",
         ]
 )
-
 app.add_page(index, route=Route.INDEX.value)
 app.add_page(login_page, route=Route.API.value)  # Ruta explícita
 
