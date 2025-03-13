c1c3a31e4c0261e448242d4e35e7cd9669914ac2
ariodev87 <ariel871207@gmail.com>
2025-03-13 12:42:58 +0100

    update

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -3,9 +3,9 @@ import reflex as rx
 config = rx.Config(
     app_name="link_bio",
     cors_allowed_origins=[
-        "http://localhost:8000/",
-        "http://localhost:3000/",
-        "https://arioweb-2.onrender.com/",
+        "http://localhost:8000",
+        "http://localhost:3000",
+        "https://arioweb-2.onrender.com",
                 ]
         )
 
