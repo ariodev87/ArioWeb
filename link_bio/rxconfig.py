e4e2326928f5510b4d9649bcdf4cbbc3fd4a1e36
ariodev87 <ariel871207@gmail.com>
2025-03-19 14:30:33 +0100

    Update rxconfig.py

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -4,9 +4,13 @@ config = rx.Config(
     app_name="link_bio",
     cors_allowed_origins=[
         "http://localhost:8000",
+        "http://localhost:8002",
         "http://localhost:3000",
-        "http://localhost:3001",
+        "http://localhost:3002",
+        "http://localhost:*",
         "https://arioweb-2.onrender.com",
+        "https://ariodev.vercel.app/",
+        
                 ]
         )
 
